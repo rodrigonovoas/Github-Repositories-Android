d228f05aeb36135c9d49181e3f0b5350b83b57e6
Rodrigo N <rodrigonovoa97s@gmail.com>
2022-04-26 23:25:33 +0200

    android project initial setup

---
+++ app/src/main/java/app/rodrigonovoa/githubrepositores/MainActivity.kt
@@ -0,0 +1,11 @@
+package app.rodrigonovoa.githubrepositores
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
