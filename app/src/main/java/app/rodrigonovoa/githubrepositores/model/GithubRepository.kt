ab141523eb74644207fec1422a24ef207f0d223a
Rodrigo N <rodrigonovoa97s@gmail.com>
2022-04-27 22:17:27 +0200

    adding repository for repository pattern

---
+++ app/src/main/java/app/rodrigonovoa/githubrepositores/model/GithubRepository.kt
@@ -0,0 +1,16 @@
+package app.rodrigonovoa.githubrepositores.model
+
+import app.rodrigonovoa.githubrepositores.repository.GithubService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import retrofit2.Response
+
+class GithubRepository(private val service: GithubService) {
+    suspend fun getRepositoriesFromApi(): Flow<Response<List<RepositoryResponse>>> = flow {
+        val repositories = service.getRepositories().execute()
+
+        emit(repositories)
+    }.flowOn(Dispatchers.IO)
+}
